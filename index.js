1fa86f7edb7479ba5630154f125ff063a1965c39
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-30 16:38:51 +0000

    Refactored to use a class

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const newTask = function(title, description) {
-  const task = {
-    title: title,
-    description: description,
-    complete: false,
+class Task {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+  }
+  logState() {
+    console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+  }
+  markCompleted() {
+    this.complete = true;
+  }
+}
 
-    logState: function() {
-      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
-    },
 
-    markCompleted: function() {
-      this.complete = true;
-    }
-  };
-  return task;
-};
-
-const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
-const task2 = newTask("Do Laundry", "😨");
+const task1 = new Task("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = new Task("Do Laundry", "😨");
 
 task1.logState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
